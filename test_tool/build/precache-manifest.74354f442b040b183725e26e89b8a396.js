08f8e5536592fd226da78fb56ebff3cf4cf8b675
Eien <shana112123@gmail.com>
2019-12-24 15:53:31 +0800

    add

---
+++ test_tool/build/precache-manifest.74354f442b040b183725e26e89b8a396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d773951c07b24937da820b486899fcb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3837006c92206fb659d1",
+    "url": "./static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "5c4af26310532f7745ca",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3837006c92206fb659d1",
+    "url": "./static/js/2.2948e086.chunk.js"
+  },
+  {
+    "revision": "14cfafeab58f243640d9d112b6ecad16",
+    "url": "./static/js/2.2948e086.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c4af26310532f7745ca",
+    "url": "./static/js/main.1179d247.chunk.js"
+  },
+  {
+    "revision": "b8f9b4ce4b9187ee0c34",
+    "url": "./static/js/runtime-main.3937b49c.js"
+  }
+]);
